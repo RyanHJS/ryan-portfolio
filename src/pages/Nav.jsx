1024e5d1bde64ab6c8b4764c1a307a8ce32d86bf
Ryan <heryanh@sfu.ca>
2025-11-26 17:03:39 -0800

    Updated nav bar
    - Added sound effec ton hover
    - Updated light switch icon

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,22 +1,42 @@
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsToggleOff, BsSunFill } from "react-icons/bs";
 
-const Nav = ({setDarkMode, darkMode}) => {
+const Nav = ({ setDarkMode, darkMode }) => {
+  const handleThemeSwitch = () => {
+    setDarkMode(!darkMode);
+    new Audio("src/assets/light-switch.mp3").play();
+  };
+
+  const playNameHoverSound = () => {
+    new Audio("src/assets/bouncy-fart.mp3").play();
+  };
+
   return (
     <div>
       <nav className="py-10 mb-12 flex justify-between dark:text-white">
-        <h1 className="font-burtons text-xl select-none">Ryan He</h1>
-        {/* TODO: change font, add bouncing animation */}
-        <ul className="flex items-center">
+        <h1
+          className="font-burtons text-xl animate-bounce hidden sm:flex"
+          onMouseEnter={playNameHoverSound}
+        >
+          Ryan He
+        </h1>
+        <ul className="flex flex-grow items-start justify-end">
           <li>
-            <BsFillMoonStarsFill
-              onClick={() => setDarkMode(!darkMode)}
-              className=" cursor-pointer text-2xl"
-            />
+            {darkMode ? (
+              <BsSunFill
+                onClick={handleThemeSwitch}
+                className="cursor-pointer text-2xl animate-[spin_3s_linear_infinite]"
+              />
+            ) : (
+              <BsToggleOff
+                onClick={handleThemeSwitch}
+                className="cursor-pointer text-2xl transition-colors hover:text-gray-500"
+              />
+            )}
           </li>
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
